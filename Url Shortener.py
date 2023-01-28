ccdf03bf0274364a960fbc4e726d7329fa12b91f
Oluwaseyi <kdom2227@gmail.com>
2023-01-28 09:42:52 +0100

    Add files via upload

--- Url Shortener.py
+++ Url Shortener.py
@@ -1,16 +1,25 @@
 import pyperclip
 import pyshorteners
-from tkinter import*
+import tkinter as tk
 
 # tkinter
-root = Tk()
-root.geometry("400x200")
+root = tk.Tk()
+root.geometry("500x300")
 root.title("URL Shortener")
-root.configure(bg= "#49a")
+root.configure(bg="#ffc3a0")
 
 # Main code
-url = StringVar()
-url_address = StringVar()
+url = tk.StringVar()
+url_address = tk.StringVar()
+
+#GUI
+header = tk.Label(root, text="My URL Shortener", font="poppins 20 bold", bg="#ffc3a0").pack(pady=15)
+link_input = tk.Entry(root, textvariable=url, width=60).pack(pady=5)
+main_btn= tk.Button(root, text="Generate Short URL", command=lambda: urlshortener(), height=2, width=20, bg="white" , fg="#ffc3a0", font="poppins 15 bold").pack(pady=7)
+link_box= tk.Entry(root, textvariable= url_address, font="25",width=40).pack(pady=5)
+
+copy_btn= tk.Button(root, text="Copy URL", command= lambda: copyurl(), height=1, width=10, bg="#20bebe" , fg="white", font="poppins 15 bold").pack(pady=5)
+
 
 def urlshortener():
     urladdress = url.get()
@@ -21,12 +30,6 @@ def copyurl():
     url_short = url_address.get()
     pyperclip.copy(url_short)
 
-#GUI
-Label(root, text="My URL Shortener", font="poppins").pack(pady=10)
-Entry(root, textvariable=url).pack(pady=5)
-Button(root, text="Generate Short URL", command=urlshortener).pack(pady=7)
-Entry(root, textvariable=url_address).pack(pady=5)
-Button(root, text="Copy URL", command=copyurl).pack(pady=5)
 
 # Tkinter activator
 root.mainloop()
